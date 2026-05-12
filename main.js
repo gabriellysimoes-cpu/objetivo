e61c855aaea9485fb3ed18956dca7b95e1c6f711
gabriellysimoes-cpu <gabrielly.simoes@escola.pr.gov.br>
2026-05-12 07:55:13 -0300

    j

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ console.log(textos)
 
 for(let i = 0); i<botoes.length;i++{
     botoes[i].onclick = fuction (){
-        
+        for(let j=0; j<botoes.length;i++){
+            botoes[j].classList.remove("ativo")
+            textos[j].classList.remove("ativo")
+        }
+        botoes[j].classList.add("ativo")
+        textos[j].classList.add("ativo")
     }
 }
