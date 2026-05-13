9b81a336390248c7f5f4abb33f6181dfb7f8a4ce
gabriellysimoes-cpu <gabrielly.simoes@escola.pr.gov.br>
2026-05-13 12:03:44 -0300

    ;

--- main.js
+++ main.js
@@ -17,12 +17,14 @@ for(let i = 0; i<botoes.length;i++){
 
  const contadores = document.querySelectorAll(".contador")
 const tempoObjetivo1 = new Date("2026-10-05T00:00:00)")
-const tempoObjetivo2 = new Date("2026-10-05T00:00:00)")
-const tempoObjetivo3 = new Date("2026-10-05T00:00:00)")
-const tempoObjetivo4  = new Date("2026-10-05T00:00:00)")
+const tempoObjetivo2 = new Date("2026-11-05T00:00:00)")
+const tempoObjetivo3 = new Date("2026-12-05T00:00:00)")
+const tempoObjetivo4  = new Date("2027-01-05T00:00:00)")
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
 function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date ();
+    let tempoFinal = temṕoObjetivo - tempoAtual
     
 }
