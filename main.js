50f990c6d54d5dcf231282c721a10cc6c35594d2
gabriellysimoes-cpu <gabrielly.simoes@escola.pr.gov.br>
2026-05-13 12:14:50 -0300

    g

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for(let i = 0; i<botoes.length;i++){
     }
 }
 
- const contadores = document.querySelectorAll(".contador")
+const contadores = document.querySelectorAll(".contador")
 const tempoObjetivo1 = new Date("2026-10-05T00:00:00)")
 const tempoObjetivo2 = new Date("2026-11-05T00:00:00)")
 const tempoObjetivo3 = new Date("2026-12-05T00:00:00)")
