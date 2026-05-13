9d602897307f5f2c5954112fac8b39f78acd05fd
gabriellysimoes-cpu <gabrielly.simoes@escola.pr.gov.br>
2026-05-13 11:59:48 -0300

    9

--- main.js
+++ main.js
@@ -20,3 +20,5 @@ const tempoObjetivo1 = new Date("2026-10-05T00:00:00)")
 const tempoObjetivo2 = new Date("2026-10-05T00:00:00)")
 const tempoObjetivo3 = new Date("2026-10-05T00:00:00)")
 const tempoObjetivo4  = new Date("2026-10-05T00:00:00)")
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
