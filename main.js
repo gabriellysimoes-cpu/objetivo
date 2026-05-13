e838bb49011073ecac35062d82748b9a75be2fba
gabriellysimoes-cpu <gabrielly.simoes@escola.pr.gov.br>
2026-05-13 12:06:37 -0300

    .

--- main.js
+++ main.js
@@ -25,6 +25,6 @@ const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date ();
-    let tempoFinal = temṕoObjetivo - tempoAtual
+    let tempoFinal = tempoObjetivo - tempoAtual
     console.log(tempoFinal)
 }
